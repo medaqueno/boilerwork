e39b045a4fe048c3415ddc7998456d8538f557a7
CarlosJ <carlos.jimenez@pangea.es>
2022-06-15 21:18:43 +0200

    Enable composer install on build if vendor folder does not exist

--- docker/rootfilesystem/customized-entrypoint.sh
+++ docker/rootfilesystem/customized-entrypoint.sh
@@ -8,4 +8,7 @@ echo "##### This line is printed out from customized-entrypoint.sh ####"
 cp /usr/local/etc/php/php.ini-development /usr/local/etc/php/conf.d/php.ini
 # cp /usr/local/etc/php/php.ini-production /usr/local/etc/php/php.ini
 
+# No vendor directory, no dependecies. Install them.
+[ ! -d "/var/www/vendor" ] && composer install || echo "vendor directory already exists"
+
 . /entrypoint.sh
