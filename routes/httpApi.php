3917e8e4c4c69c6ba47c2a52412c893f0ba53f41
CarlosJ <carlos.jimenez@pangea.es>
2022-07-07 23:00:58 +0200

    Chore: Remove Middleware from example port by default

--- routes/httpApi.php
+++ routes/httpApi.php
@@ -7,6 +7,6 @@ use App\Core\BC\UI\Ports\Http\ExamplePort;
 use Boilerwork\System\Http\Middleware\NeedsAuthInfoMiddleware;
 
 return [
-    ['POST', '/registerUser', ExamplePort::class, [NeedsAuthInfoMiddleware::class]],
-    // ['POST', '/registerUser', ExamplePort::class, []],
+    // ['POST', '/registerUser', ExamplePort::class, [NeedsAuthInfoMiddleware::class]],
+    ['POST', '/registerUser', ExamplePort::class, []],
 ];
