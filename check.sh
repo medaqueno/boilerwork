e7e99cbc601774bfcd17ffa1ba489b98fcd09532
CarlosJ <carlos.jimenez@pangea.es>
2022-07-11 18:52:15 +0200

    Chore: Improve check composer update and change default name for event sourcing database

--- check.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-[[ ! $(composer validate --no-check-publish) =~ "is not valid" ]] || echo "composer.json and composer.lock are synced"
--- docker/rootfilesystem/customized-entrypoint.sh
+++ docker/rootfilesystem/customized-entrypoint.sh
@@ -9,9 +9,9 @@ cp /usr/local/etc/php/php.ini-development /usr/local/etc/php/conf.d/php.ini
 # cp /usr/local/etc/php/php.ini-production /usr/local/etc/php/php.ini
 
 # No vendor directory, no dependencies. Install them.
-[ ! -d "/var/www/vendor" ] && composer install || echo "vendor directory already exists"
-[[ ! $(composer validate --no-check-publish) =~ "is not valid" ]] && composer update || echo "composer.json and composer.lock are synced"
-[ ! -f "/var/www/.env" ] && cp .env.local .env || echo ".env already exists"
+[ ! -d "/var/www/vendor" ] && composer install || echo "Composer: vendor directory already exists"
+[[ $(composer validate --no-check-publish) = "./composer.json is valid" ]] && echo "Composer: composer.json and composer.lock are synced" || composer update
+[ ! -f "/var/www/.env" ] && cp .env.local .env || echo "Env file: .env already exists"
 
 [ ! -d "/var/www/logs" ] && mkdir logs
 
